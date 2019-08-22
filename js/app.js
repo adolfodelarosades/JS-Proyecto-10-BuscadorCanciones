4a62c70c525f5d1efd423bc9e7ebed206377cf56
Adolfo <adolfodelarosa2012@gmail.com>
2019-08-22 12:18:08 +0200

    Validando el formulario de búsqueda

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
 import * as UI from './interfaz.js';
 
-console.log(UI);
+//console.log(UI);
+UI.formularioBuscar.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //Obtener datos del formulario
+    const artista = document.querySelector('#artista').value,
+          cancion = document.querySelector('#cancion').value;
+
+    if(artista === '' || cancion === ''){
+        //El usuario deja los campos vacios, mostrar error        
+        UI.divMensaje.innerHTML = 'Error todos los campos son obligatorios';
+        UI.divMensaje.classList.add('error');
+        setTimeout(() => {  
+            UI.divMensaje.innerHTML = '';
+            UI.divMensaje.classList.remove('error');
+        }, 3000);
+    }else{
+        //El formulario esta completo, realizar consulta a la API
+        
+    }
+});
+
+
 
